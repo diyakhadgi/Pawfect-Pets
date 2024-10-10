c376193c934b447b9d052c8f6eda3ad8c246b4f5
diyakhadgi <khadgidiya8@gmail.com>
2024-10-10 20:16:10 +0545

    navbar added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <ul className="flex justify-end space-x-6 px-5 py-5 bg-cyan-900 text-white p">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Adopt</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
