f85f6bb3890d807e4a86d8c4d7aac88d8e7c16d5
diyakhadgi <khadgidiya8@gmail.com>
2024-10-17 14:19:00 +0545

    new branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar"
+import Login from "./pages/Login";
 
 function App() {
 
   return (
     <>
-     <Navbar/>
+      <Navbar />
+      <Login/>
     </>
   )
 }
