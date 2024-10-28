6d4dbfd1805e9135ad3d037d247cb68045f49801
Shinu-ster <sajatbazz@gmail.com>
2024-10-28 17:22:48 +0545

    Implemented React Query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
-import MyRoutes from './routes/MyRoutes'
+import MyRoutes from "./routes/MyRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime:10000
+    }
+  }
+});
 function App() {
-
   return (
     <>
-      <MyRoutes/>
+      <QueryClientProvider client={queryClient}>
+        <MyRoutes />
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
